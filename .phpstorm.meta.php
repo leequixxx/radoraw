b3c71f98557be4ac9366b96361314637f25d18d9
Misha Vorobeychikov <mail@leequixxx.space>
2019-11-04 22:47:56 +0300

    feat: installed docker/dbal

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-11-04 18:19:58.
+    * Generated on 2019-11-04 21:52:49.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.4.1 on 2019-11-04 18:19:45.
+ * Generated for Laravel 6.4.1 on 2019-11-04 21:52:49.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
