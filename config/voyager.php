54a37cf3d937c62c505ce01c157223ebdfcc0cd2
Misha Vorobeychikov <mail@leequixxx.space>
2019-11-04 19:08:22 +0300

    feat: added russian localization and created element bread

--- config/voyager.php
+++ config/voyager.php
@@ -181,7 +181,7 @@ return [
     |
     */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#eb3b5a',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
